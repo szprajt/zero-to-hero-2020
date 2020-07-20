a72c896c0c5992229bb07d9d1ddccbb987af1e96
David Kodela <kodela93@hotmail.hu>
2020-07-20 18:54:15 +0200

    test crud endpoint

--- src/main/java/hu/zerotohero/verseny/crud/CrudController.java
+++ src/main/java/hu/zerotohero/verseny/crud/CrudController.java
@@ -2,7 +2,11 @@ package hu.zerotohero.verseny.crud;
 
 import hu.zerotohero.verseny.crud.model.LocationRequest;
 import hu.zerotohero.verseny.crud.model.LocationResponse;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/crud")
@@ -11,9 +15,9 @@ public class CrudController {
     //TODO implement endpoints
 
     @PostMapping
-    @RequestMapping(value = "/api/location/", headers = "Accept=application/json")
-    public @ResponseBody
-    LocationResponse persistLocation(@RequestBody final LocationRequest locationRequest) {
+    @RequestMapping(value = "/api/location/", headers = "Accept=application/json", consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public LocationResponse persistLocation(@RequestBody final LocationRequest locationRequest) {
         return new LocationResponse().setAddress(locationRequest.getAddress()).setName(locationRequest.getName());
     }
 }
