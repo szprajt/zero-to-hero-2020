da5d32983dd490f82cb90ab27cf8b6bfb7a0f594
David Kodela <kodela93@hotmail.hu>
2020-07-21 18:47:04 +0200

    test equipment endpoint

--- src/main/java/hu/zerotohero/verseny/crud/entity/Employee.java
+++ src/main/java/hu/zerotohero/verseny/crud/entity/Employee.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+@Entity
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
--- src/main/java/hu/zerotohero/verseny/crud/repository/EmployeeRepository.java
+++ src/main/java/hu/zerotohero/verseny/crud/repository/EmployeeRepository.java
@@ -4,11 +4,9 @@ import hu.zerotohero.verseny.crud.entity.Employee;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
     @Query(value = "SELECT * FROM Employee e WHERE e.location_id = :locationId")
