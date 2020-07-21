456c087fd642e28653649d0ff303e3cc5f9dc001
David Kodela <kodela93@hotmail.hu>
2020-07-21 19:04:29 +0200

    test equipment endpoint

--- src/main/java/hu/zerotohero/verseny/crud/repository/EmployeeRepository.java
+++ src/main/java/hu/zerotohero/verseny/crud/repository/EmployeeRepository.java
@@ -1,6 +1,7 @@
 package hu.zerotohero.verseny.crud.repository;
 
 import hu.zerotohero.verseny.crud.entity.Employee;
+import hu.zerotohero.verseny.crud.entity.Location;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +10,6 @@ import java.util.List;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
-    @Query(value = "SELECT * FROM Employee e WHERE e.location_id = :locationId")
-    List<Employee> findByWorksAt(@Param("locationId") long locationId);
+    List<Employee> findByWorksAt(Location location);
+    
 }
