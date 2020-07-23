a0dfae17ae162d4b7918fb7b32291e1f689046c5
David Kodela <kodela93@hotmail.hu>
2020-07-23 22:24:39 +0200

    test equipment endpoint

--- src/main/java/hu/zerotohero/verseny/crud/validator/WorkerValidator.java
+++ src/main/java/hu/zerotohero/verseny/crud/validator/WorkerValidator.java
@@ -2,6 +2,7 @@ package hu.zerotohero.verseny.crud.validator;
 
 import hu.zerotohero.verseny.crud.entity.Employee;
 import hu.zerotohero.verseny.crud.model.Job;
+import hu.zerotohero.verseny.crud.model.Type;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -24,6 +25,12 @@ public class WorkerValidator implements Validatable {
             if (Objects.nonNull(employee.getOperates().getLocatedAt()) && employee.getWorksAt().getId() != employee.getOperates().getLocatedAt().getId()) {
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Equipment should be on the same location as the working place");
             }
+        }
+        if (Job.CASHIER.equals(employee.getJob()) && !Type.CASH_REGISTER.equals(employee.getOperates().getType())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cashier cnly work with cash register");
+        }
+        if (Job.COOK.equals(employee.getJob()) && !Type.OVEN.equals(employee.getOperates().getType())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cook cnly work with oven");
         }
     }
 }
