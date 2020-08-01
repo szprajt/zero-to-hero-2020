2f92b0d26054299411517062eee057897ca2d4db
David Kodela <kodela93@hotmail.hu>
2020-08-01 18:51:36 +0200

    created stairs controller'

---
+++ src/main/java/hu/zerotohero/verseny/stairs/StairsController.java
@@ -0,0 +1,38 @@
+package hu.zerotohero.verseny.stairs;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@RestController
+public class StairsController {
+
+
+    @GetMapping("/number-of-steps/getNumberOfSteps")
+    public String getNumberOfSteps(@RequestParam(value = "numberOfStair") Integer numberOfStair,
+                                   @RequestParam(value = "stepSizeList", defaultValue = "1") List<Integer> stepSizeList) {
+        if (numberOfStair < 1) {
+            return "At least 1 stair need";
+        }
+        Set<Integer> stepSizeSet = new HashSet<>(stepSizeList);
+        Integer[] array = Arrays.copyOf(stepSizeSet.toArray(), stepSizeSet.size(), Integer[].class);
+        int[] primitiveArray = Arrays.stream(array).mapToInt(Integer::intValue).toArray();
+        return countWays(numberOfStair, primitiveArray, primitiveArray.length) + " way is possible";
+    }
+
+
+    private int countWays(int n, int[] arr, int len) {
+        int noWays = 0;
+        for (int i = 0; i < len; i++) {
+            if (n - arr[i] >= 0) {
+                noWays += countWays(n - arr[i], arr, len);
+            }
+        }
+        return noWays;
+    }
+}
